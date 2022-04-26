8bfda5835a0685db023829b70f130d1120351b0d
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-04-26 14:52:19 +0300

    Adding IRepository And CustomerRepo

---
+++ CustomerPanelAssignment.API/DataAccess/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using CustomerPanelAssignment.API.Models;
+using DataAccess.Data;
+using DataAccess.Repositories.IRepository;
+using System;
+
+namespace DataAccess.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CustomerRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Customer obj)
+        {
+            _db.Customer.Update(obj);
+        }
+    }
+}
---
+++ CustomerPanelAssignment.API/DataAccess/Repositories/IRepository/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using CustomerPanelAssignment.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.IRepository
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        void Update(Customer obj);
+    }
+}
