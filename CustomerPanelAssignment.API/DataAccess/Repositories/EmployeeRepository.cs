5a196e58cd06dd05521ce5cdbac9470b27fbcbe4
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-04-26 14:57:59 +0300

    Adding customer repo

--- CustomerPanelAssignment.API/DataAccess/Repositories/EmployeeRepository.cs
+++ CustomerPanelAssignment.API/DataAccess/Repositories/EmployeeRepository.cs
@@ -5,17 +5,17 @@ using System;
 
 namespace DataAccess.Repositories
 {
-    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public EmployeeRepository(ApplicationDbContext db) : base(db)
+        public CustomerRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
-        public void Update(Employee obj)
+        public void Update(Customer obj)
         {
-            _db.Employee.Update(obj);
+            _db.Customer.Update(obj);
         }
     }
 }
