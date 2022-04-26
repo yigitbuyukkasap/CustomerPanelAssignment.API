663a5da67be21d8fb85b00d674f7801eaf556d9f
yigitbuyukkasap <yigitbyksp@gmail.com>
2022-04-26 13:38:42 +0300

    Adding DbContext in DataAccess Folder

--- CustomerPanelAssignment.API/CustomerPanelAssignment.API/Startup.cs
+++ CustomerPanelAssignment.API/CustomerPanelAssignment.API/Startup.cs
@@ -1,7 +1,9 @@
+using CustomerPanelAssignment.API.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,13 @@ namespace CustomerPanelAssignment.API
         {
 
             services.AddControllers();
+
+            // Adding DbContext
+            services.AddDbContext<CustomerPanelContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("CustomerPanelDb")));
+
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CustomerPanelAssignment.API", Version = "v1" });
